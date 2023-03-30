2c835b44cddec5f0fe1c026b13a5c5c6976f534c
vanessa cristian da silva pinheiro <vanessa_cristyan@hotmail.com>
2023-03-30 14:18:30 -0300

    iniciando aula sobre LifeCycle

--- LifeCycle/LifeCycle/ViewController.swift
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -1,20 +1,19 @@
 //
 //  ViewController.swift
-//  financeapp-treinando
+//  LifeCycle
 //
-//  Created by Usuario on 18/03/23.
+//  Created by Usuario on 30/03/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var homeScreenViewController: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        // Do any additional setup after loading the view.
     }
-   
+
+
 }
 
