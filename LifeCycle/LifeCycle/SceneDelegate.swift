a08b33a238385f37bed862167c907b855bb2a0e4
vanessa cristian da silva pinheiro <vanessa_cristyan@hotmail.com>
2023-03-30 14:23:42 -0300

    codigo da aula

--- LifeCycle/LifeCycle/SceneDelegate.swift
+++ LifeCycle/LifeCycle/SceneDelegate.swift
@@ -7,46 +7,33 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    class ViewController: UIViewController {
+        
+        // Método que é disparado quando a tela está prestes a ser apresentada. - configurar parte navigation, configurar animação
+        override func viewWillAppear(_ animated: Bool) {
+            print(#function)
+        }
+        
+        // O metodo viewDidLoad é disparado assim que toda a tela for renderizada, sendo assim, é disparado uma única vez. - configurar elementos (configurar fonte, cor, estilo, disparar request de inicio, etc...)
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            print(#function)
+        }
 
-    var window: UIWindow?
+        // Método viewDidAppear é disparado toda a vez que a tela é apresentada por completo. - configurar animação e timer
+        override func viewDidAppear(_ animated: Bool) {
+            print(#function)
+        }
 
+        // Método que é disparado quando a tela está prestes a sair. - configurar animação e timer
+        override func viewWillDisappear(_ animated: Bool) {
+            print(#function)
+        }
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // Método que é disparado quando a tela já saiu. - configurar animação e timer
+        override func viewDidDisappear(_ animated: Bool) {
+            print(#function)
+        }
+        
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
-}
 
--- LifeCycle/LifeCycle/ViewController.swift
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class Tela02ViewController: UIViewController {
+    
+    // Método que é disparado quando a tela está prestes a ser apresentada. - configurar parte navigation, configurar animação
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    // O metodo viewDidLoad é disparado assim que toda a tela for renderizada, sendo assim, é disparado uma única vez. - configurar elementos (configurar fonte, cor, estilo, disparar request de inicio, etc...)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(#function)
     }
-
-
+    
+    // Método viewDidAppear é disparado toda a vez que a tela é apresentada por completo. - configurar animação e timer
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    // Método que é disparado quando a tela está prestes a sair. - configurar animação e timer
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    // Método que é disparado quando a tela já saiu. - configurar animação e timer
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
 }
-
