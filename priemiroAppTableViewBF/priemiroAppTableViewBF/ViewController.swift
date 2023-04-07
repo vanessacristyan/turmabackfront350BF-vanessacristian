e5947ff66fd644c6e64db4b7556d50d8fc1fd03a
vanessa cristian da silva pinheiro <vanessa_cristyan@hotmail.com>
2023-04-06 22:17:16 -0300

    add o primeiro app table view

---
+++ priemiroAppTableViewBF/priemiroAppTableViewBF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  priemiroAppTableViewBF
+//
+//  Created by Usuario on 06/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
